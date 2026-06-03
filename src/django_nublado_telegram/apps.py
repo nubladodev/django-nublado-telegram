d9ff72c09961b385563519f6a20098295726c0a9
Nublado <nublado.dev@gmail.com>
2026-06-03 14:14:17 -0700

    Initial init

---
+++ src/django_nublado_telegram/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoTelegramConfig(AppConfig):
+    name = "django_telegram"
---
+++ src/django_nublado_telegram/management/commands/set_bot_webhooks.py
@@ -0,0 +1,17 @@
+import asyncio
+import logging
+
+from django.core.management.base import BaseCommand
+
+from django_telegram.bot_registry import registry
+
+logger = logging.getLogger("django")
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        asyncio.run(self.run())
+
+    async def run(self):
+        for bot in registry.all():
+            await bot.ensure_webhook()
