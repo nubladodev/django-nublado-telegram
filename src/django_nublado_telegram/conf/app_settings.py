92217b446acdf7caad6a94b693f84e80e5cb871b
Nublado <nublado.dev@gmail.com>
2026-06-07 18:57:52 -0700

    ...

---
+++ src/django_nublado_telegram/conf/app_settings.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+from django.conf import settings as django_settings
+
+from django_nublado_core.conf.base import AppSettings
+
+# The app's settings dict name
+SETTINGS_DICT_NAME = "DJANGO_NUBLADO_TELEGRAM"
+
+# The app settings default values.
+SETTINGS_DEFAULTS = {"SOURCE_LANGUAGE": django_settings.LANGUAGE_CODE}
+
+
+@dataclass(frozen=True)
+class AppData:
+    SOURCE_LANGUAGE: str
+
+
+app_settings = AppSettings(
+    defaults=SETTINGS_DEFAULTS,
+    settings_dict_name=SETTINGS_DICT_NAME,
+    cls=AppData,
+)
